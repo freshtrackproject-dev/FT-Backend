0eaab19d992b42e8acffdc06e24392e60ad48cbc
Mvince101 <mvgapaz11.mv@gmail.com>
2025-10-13 10:39:18 +0800

    update

--- server.js
+++ server.js
@@ -138,6 +138,6 @@ setInterval(() => {
 // ==============================
 // 🚀 Start server
 // ==============================
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
 });
